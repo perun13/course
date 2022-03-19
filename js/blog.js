f22e092d82ed3e7e53d7a8f7f57fe15ab04b9dce
perun13 <97544170+perun13@users.noreply.github.com>
2022-03-19 16:05:08 +0200

    Add files via upload

---
+++ js/blog.js
@@ -0,0 +1,43 @@
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android()
+            || isMobile.BlackBerry()
+            || isMobile.iOS()
+            || isMobile.Opera()
+            || isMobile.Windows()
+        );
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+}
+
+const headerBurger = document.querySelector('.header_burger');
+const navBurger = document.querySelector('.nav_burger');
+if (headerBurger) {
+
+    headerBurger.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        headerBurger.classList.toggle('_active');
+        navBurger.classList.toggle('_active');
+    });
+};
